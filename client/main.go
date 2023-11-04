b79d3b7da91bce6eecd63364a289e3281385a2c3
Rodrigo A <rodrigo.ancavil@cornershopapp.com>
2023-11-04 00:48:40 -0300

    grpc(microservice): modify client to use transport credentials

--- client/main.go
+++ client/main.go
@@ -8,10 +8,11 @@ import (
 	pb "grpc/ms/pb"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
-	opts := grpc.WithInsecure()
+	opts := grpc.WithTransportCredentials(insecure.NewCredentials())
 	cc, err := grpc.Dial("localhost:9999", opts)
 	if err != nil {
 		log.Fatal(err)
@@ -19,7 +20,7 @@ func main() {
 	defer cc.Close()
 
 	client := pb.NewPaymentServiceClient(cc)
-	request := &pb.PaymentServiceRequest{Id: "2036"}
+	request := &pb.PaymentServiceRequest{Id: "21111036"}
 
 	resp, err := client.PayTo(context.Background(), request)
 
